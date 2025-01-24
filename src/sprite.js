f510310fddcd9822977c2d44d79992b960227796
Martial Grin <49276099+martialgrin@users.noreply.github.com>
2025-01-24 13:21:32 +0100

    change images and add analytics

---
+++ src/sprite.js
@@ -0,0 +1,27 @@
+import { Sprite } from "pixi.js";
+
+export const spriteElement = (images, x, y, scale, imagesNum) => {
+	let imageNum = 0;
+	let image = images[imagesNum];
+	const sprite = new Sprite({
+		texture: image,
+		x,
+		y,
+		scale: scale,
+		anchor: 0.5,
+	});
+	const updateImage = () => {
+		imageNum = (imageNum + 1) % images.length;
+		sprite.texture = images[imageNum];
+	};
+
+	const loop = (time) => {
+		sprite.rotation = time * 0.001;
+	};
+
+	return {
+		sprite,
+		updateImage,
+		loop,
+	};
+};
